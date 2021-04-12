6d7ba4a48fcbe0db6c3442d14b33fe53f6f93050
SabriSomrani <nasrimehdi96@gmail.com>
2021-04-12 12:20:11 +0100

    add exercice 2

---
+++ exercice 2/exericce2.js
@@ -0,0 +1,142 @@
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Reading  a user from a database...');
+        callback({ id: id, gitHubUsername: 'abc' });
+    }, 2000)
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Calling GitHub API...');
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}
+
+
+function getBrunch(repo, callback) {
+    setTimeout(() => {
+        console.log('Getting commits for a GitHub repo...');
+        callback(['commit1', 'commit2', 'commit3']);
+    }, 2000)
+}
+
+function postCommit(n, callback) {
+    let committed = true; // code de get commited or not 
+    if (committed) {
+        let data = "commited";
+        callback(data);
+    } else {
+        let data = "not commited";
+        callback(data);
+    }
+
+}
+
+getuserfinich = (id) => {
+    getUser(id, getRepositoriesFinish)
+}
+
+getRepositoriesFinish = (b) => {
+    getRepositories(b, getBrunchFinish)
+}
+
+getBrunchFinish = (c) => {
+    getBrunch(c, postCommitFinish)
+}
+
+postCommitFinish = (d) => {
+    postCommit(d, console.log)
+}
+
+getuserfinich(5)
+
+
+//promise 
+getUser = (a) => {
+    return new Promise((resolve, reject) => {
+
+        //code de get USER
+        console.log("Get USER")
+        let user = 'ssasa';
+        if (user !== null)
+            resolve(user.gitHubUsername)
+        else
+            reject('errerur importaton de User')
+    })
+}
+getRepositories = (b) => {
+
+    return new Promise((resolve, reject) => {
+        //code de get repos and usernameS
+        console.log("get Respository")
+        let repos = new Array("execice1", "execice2");
+        let levelNumber = 0;
+        if (repos !== null && levelNumber !== null) {
+            resolve([repos, levelNumber])
+        } else {
+            reject("erreur niveau function get Repository !!!")
+        }
+
+    })
+}
+
+getBrunch = (c) => {
+    return new Promise((resolve, reject) => {
+
+        let brunch = "master"; //code de get Brunch (brunch == ?)
+        console.log("get Brunch")
+        if (brunch !== null)
+            resolve(brunch)
+        else
+            reject("erreur de brunch !!!")
+
+
+    })
+}
+
+postCommit = (d) => {
+
+    return new Promise((resolve, reject) => {
+        let committed = true
+        console.log("post Commit")
+        if (d === "master") {
+            if (committed)
+                resolve("The new version is Commited")
+            else
+                resolve("The new version is not commited")
+        } else {
+            reject("on n'est pas dans la brunch master")
+        }
+
+    })
+}
+
+// execute promise
+getUser(1)
+    .then(getRepositories)
+    .then(getBrunch)
+    .then(postCommit)
+    .then(console.log)
+    .catch(console.log)
+
+
+
+/// await - async 
+
+getuser2 = async(a) => {
+    try {
+        let user = { 'gitHubUsername': "mehdi" } //code user
+
+        let [repos, levelNumber] = await getRepositories(user.gitHubUsername);
+
+        brunch = await getBrunch(repos[levelNumber]);
+        verif = await postCommit(brunch)
+        console.log(verif)
+    } catch (e) {
+        console.log(e)
+    }
+
+
+}
+
+getuser2(1)
